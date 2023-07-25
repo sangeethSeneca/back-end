1dfa8de86260ea00a3c96e381957d299bcf1dd28
sangeeth-perera <raviperera93@gmail.com>
2023-07-24 21:34:11 -0400

    changes

--- modules/collegeData.js
+++ modules/collegeData.js
@@ -5,7 +5,7 @@ var sequelize = new Sequelize(
   "klnksjno",
   "W96p_9jIklCE5HY0mBB9Vi7V1GR676YR",
   {
-    host: "salt.db.elephantsql.com1",
+    host: "salt.db.elephantsql.com",
     dialect: "postgres",
     port: 5432,
     dialectOptions: {
